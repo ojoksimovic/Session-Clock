c0fac8cb43e4c8e38fd14ac9db42605ccfe58cd6
olivera <69132868+ojoksimovic@users.noreply.github.com>
2020-12-31 12:17:15 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello! Olivera is currently crafting this app together. Stay Tuned!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
