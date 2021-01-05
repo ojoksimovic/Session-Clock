56a6c86c9a9697bca7cedec622af01028eb87719
olivera <69132868+ojoksimovic@users.noreply.github.com>
2021-01-04 22:10:55 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SessionClock from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SessionClock />
   </React.StrictMode>,
   document.getElementById('root')
 );
